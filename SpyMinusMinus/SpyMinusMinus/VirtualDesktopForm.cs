1b597de79c6856730c5593279b460e9498b55424
whilow <schockwil@gmail.com>
2019-11-11 16:20:13 -0700

    virtual desktop scaling

--- SpyMinusMinus/SpyMinusMinus/VirtualDesktopForm.cs
+++ SpyMinusMinus/SpyMinusMinus/VirtualDesktopForm.cs
@@ -15,6 +15,7 @@ namespace SpyMinusMinus {
         private WindowManager windowManager;
 
         private Rectangle virtualDesktop, scaledDesktop;
+        private float scale;
 
         public VirtualDesktopForm() {
             InitializeComponent();
@@ -27,9 +28,16 @@ namespace SpyMinusMinus {
                 Console.WriteLine($"{screen.DeviceName}, {screen.Primary}, {screen.Bounds}");
             }
             Console.WriteLine("Virtual screen: " + SystemInformation.VirtualScreen);
-            virtualDesktop = new Rectangle(0, 0, SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
-            //virtualDesktop = new Rectangle(0, 0, screens[0].Bounds.Width, screens[0].Bounds.Height);
+            //todo: handle multiply monitors and window placement 
+            //virtualDesktop = new Rectangle(, 0, SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+            virtualDesktop = new Rectangle(0, 0, screens[0].Bounds.Width, screens[0].Bounds.Height);
             ResizeDesktop();
+
+
+            Timer refreshTimer = new Timer();
+            refreshTimer.Interval = 100;
+            refreshTimer.Tick += (e, s) => Invalidate();
+            refreshTimer.Start();
         }
 
         protected override void OnPaint(PaintEventArgs e) {
@@ -37,38 +45,64 @@ namespace SpyMinusMinus {
         }
 
         private void Update(Graphics g) {
+            //var time = DateTime.Now;
+
             windowManager.EnumerateWindows();
-            Random rng = new Random();
-
-            g.FillRectangle(Brushes.Black, ClientRectangle);
-
-            foreach (VirtualWindow window in windowManager.GetWindows()) {
-                NativeMethods.RECT r = window.GetWindowRect();
-                Rectangle rect = new Rectangle {
-                    X = r.Left,
-                    Y = r.Bottom,
-                    Width = r.Right,
-                    Height = r.Top
-                };
-
-                //todo: scale windows relative to scaled desktop
-                g.DrawRectangle(new Pen(Color.FromArgb(rng.Next(255), rng.Next(255), rng.Next(255))), rect);
-
-            }
-            g.DrawEllipse(Pens.Red, ClientRectangle);
 
             
+            g.FillRectangle(Brushes.White, ClientRectangle);
+            g.DrawRectangle(Pens.DarkGray, DisplayRectangle);
+
+            
+            g.FillRectangle(Brushes.Black, scaledDesktop);
             g.DrawRectangle(Pens.White, scaledDesktop);
             g.DrawEllipse(Pens.DarkGray, scaledDesktop);
+
+            /*
+            VirtualWindow window = windowManager.GetWindows().Find(w => w.GetWindowText.Contains("Untitled - Notepad"));
+            if (window != null) {
+                NativeMethods.RECT windowRect = window.GetWindowRect();
+                Rectangle scaledWindow = ScaleWindowToVirtualDesktop(windowRect, scaledDesktop, scale);
+
+                g.DrawRectangle(Pens.Red, scaledWindow);
+            }*/
+
+            foreach (VirtualWindow window in windowManager.GetWindows()) {
+                NativeMethods.RECT windowRect = window.GetWindowRect();
+                Rectangle scaledWindow = ScaleWindowToVirtualDesktop(windowRect, scaledDesktop, scale);
+
+                g.DrawRectangle(Pens.Red, scaledWindow);
+
+                //Random rng = new Random();
+                //g.DrawRectangle(new Pen(Color.FromArgb(rng.Next(255), rng.Next(255), rng.Next(255))), scaledWindow);
+            }
+
+            //Console.WriteLine("update & render: " + DateTime.Now.Subtract(time).Milliseconds);
+        }
+
+        private static Rectangle ScaleWindowToVirtualDesktop(NativeMethods.RECT windowRect, Rectangle desktopRect, float desktopScale) {
+            //convert native rect to drawing rect
+            Rectangle scaledRect = new Rectangle(windowRect.Left, windowRect.Bottom, windowRect.Right - windowRect.Left, windowRect.Bottom - windowRect.Top);
+
+            //scale
+            scaledRect.X = (int)(scaledRect.X * desktopScale);
+            scaledRect.Y = (int)(scaledRect.Y * desktopScale);
+            scaledRect.Width = (int)(scaledRect.Width * desktopScale);
+            scaledRect.Height = (int)(scaledRect.Height * desktopScale);
+
+            //offset position based on desktop location
+            scaledRect.X += desktopRect.X;// - scaledRect.Width;
+            scaledRect.Y += desktopRect.Y - scaledRect.Height;
+
+            return scaledRect;
         }
 
         private void ResizeDesktop() {
             float scaleHor = (float)DisplayRectangle.Width / (float)virtualDesktop.Width;
             float scaleVert = (float)DisplayRectangle.Height / (float)virtualDesktop.Height;
             bool vert = (scaleVert >= scaleHor);
-            float scale = vert ? scaleHor : scaleVert;
 
-
+            scale = vert ? scaleHor : scaleVert;
             scaledDesktop = new Rectangle(0, 0, (int)(virtualDesktop.Width * scale) - 1, (int)(virtualDesktop.Height * scale) - 1);
             if (vert) {
                 scaledDesktop.Y += DisplayRectangle.Height / 2 - scaledDesktop.Height / 2;
@@ -77,11 +111,12 @@ namespace SpyMinusMinus {
             }
             if (Math.Abs(scaleHor - scaleVert) < 0.001f) {
                 //epsilon check, if scale is close enough just set origin 0,0 
-                // to keep rect within client draw area
+                //to keep rect within client draw area
                 scaledDesktop.X = 0;
                 scaledDesktop.Y = 0;
             }
-            Console.WriteLine($"{scaleHor} - {scaleVert}: {scaledDesktop}");
+
+            Console.WriteLine($"{scale} ({scaleHor} - {scaleVert}): {scaledDesktop}");
         }
 
         private void VirtualDesktopForm_Resize(object sender, EventArgs e) {
